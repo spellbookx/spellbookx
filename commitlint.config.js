67ef606d3b4a0f0684ee66bcca70de9f7b0785e5
Davide Di Criscito <davide.dicriscito@gmail.com>
2025-06-07 10:07:24 +0200

    chore: update commitlint configuration to include ignores for fixup and squash commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (commit) => commit.startsWith('fixup!') || commit.startsWith('squash!'),
+  ],
+};
